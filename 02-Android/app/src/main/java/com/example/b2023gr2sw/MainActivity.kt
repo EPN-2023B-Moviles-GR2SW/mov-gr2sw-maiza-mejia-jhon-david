c75fc7b2a541a84b2227aea07f203a655f663db9
JhonDMM <jhon.maiza@epn.edu.ec>
2024-01-04 00:50:48 -0500

    Creacion actividad para CRUD

--- 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
+++ 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
@@ -65,6 +65,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Base de datos sqlite
+        EBaseDeDatos.tablaEntrenador = ESqliteHelperEntrenador(
+            this
+        )
+
         val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
         botonCicloVida.setOnClickListener{
             irActividad(ACicloVida::class.java)
@@ -91,6 +96,12 @@ class MainActivity : AppCompatActivity() {
         botonIntenExplicito.setOnClickListener {
             abrirActivityConParametros(CIntentExplicitoParametros::class.java)
         }
+
+        val botonSqlite = findViewById<Button>(R.id.btn_sqlite)
+        botonSqlite
+            .setOnClickListener {
+                irActividad(ECrudEntrenador::class.java)
+            }
     }
 
     fun abrirActivityConParametros(
